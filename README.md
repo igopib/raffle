6733db3d68725b543ef7e431edadc1c4299f5cfc
Gurpreet Singh <gopinho@Gurpreets-MacBook-Air.local>
2022-08-22 23:53:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Raffle Smart Contract
+
+This smart contract will take ETH as a payment to assign tickets to users. Later after certaint time will use chainlink random number to pick a winner.
---
+++ contracts/Raffle.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+error Raffle__NotEnoughETH();
+
+contract Raffle {
+    
+    // State variables 
+    uint256  private s_ticketPrice;
+    address payable[] private s_players;
+
+    constructor(uint256 ticketPrice) {
+        s_ticketPrice = ticketPrice;
+    }
+
+    function enterRaffle()  public payable {
+        if(msg.value < s_ticketPrice){revert Raffle__NotEnoughETH();}
+        s_players.push(payable(msg.sender));
+    }
+
+    // function pickWinner() private {
+
+    // }
+
+    // Function returns the price set the ticket price set by the deployer
+    function getTicketPrice() public view returns(uint256) {
+        return s_ticketPrice;
+    }
+
+    // Function returns the player using their index in array
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
+}
