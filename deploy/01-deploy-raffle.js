1dfe2aec0b5a53b19a489c2b99891331b676bac5
Gurpreet <gs109690@gmail.com>
2022-08-27 22:15:33 -0500

    deploy scripts

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,27 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract(
+            "VRFCoordinatorV2mock"
+        )
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordiantorV2"]
+    }
+
+    const enteranceFee = networkConfig[chainId]["enternaceFee"]
+    const args = [vrfCoordinatorV2Address, enteranceFee]
 
     const raffle = await deploy("Raffle", {
         from: deploy,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blocConfirmations || 1,
     })
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,15 @@
+const { ethers } = require("hardhat")
+
+// Enternance fee can be edited according to which network its being deployed on.
 const networkConfig = {
     4: {
         name: "rinkeby",
         vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        enteranceFee: ethers.utils.parseEther("0.02"),
+    },
+    31337: {
+        name: "hardhat",
+        enteranceFee: ethers.utils.parseEther("0.02"),
     },
 }
 
